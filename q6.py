7a092df1dbf108d7436d84f41a64c68e3434abbd
Py-Pioneers <pypioneers2026@gmail.com>
2026-02-16 19:04:53 +0530

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,8 @@
+#Team name : Py-Pioneers
+def convert_temperature(value, unit):
+    if unit== 'C':
+        return round((value * 9/5) + 32, 1)
+    elif unit== 'F':
+        return round((value - 32) * 5/9, 1)
+    else:
+        return "Invalid Unit"
